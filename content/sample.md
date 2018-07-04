7a0a5fa22f78dd579b5c52e446d630ffb814c3b3
Mandaris <info@mandarismoore.com>
2018-07-03 22:34:48 -0700

    Create Example of for use
    A simple example of the down header markdown plugin.

---
+++ content/sample.md
@@ -0,0 +1,22 @@
+title: Pelican Header Testings  
+date: 2030-07-03 14:05:49  
+category: blogging  
+tags: html, heading, pelican  
+status: published    
+
+
+I wanted to make sure that the headings that I put in my articles are the correct syntax. I'll put this project up on my blog so that others can find it as well.
+
+# First Heading
+
+This is text found underneath the first heading. This might as well be lorem ipsom but I wanted to work on my typing.
+
+
+## Second Heading
+Some more text here. Remember, the quick brown fox jumped over the lazy dog!
+
+### Third Heading
+Finer point. I don't know how many poeple go this far. Everything in this world is magic, except to the magician.
+
+#### Forth 
+I'm all the way down now. I can see the bottom.
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Mandaris'
+SITENAME = u'Pelican Headers'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = u'en'
+
+THEME = 'simple'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+
+# Markdown Plugins
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+        'downheader': {'levels': '2'},
+    },
+    'output_format': 'html5',
+}
